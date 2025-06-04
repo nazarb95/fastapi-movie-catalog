83f65701e4281600d78c77b2499a61fbbdae152d
Nazar <naz95naz@gmail.com>
2025-06-05 01:28:30 +0300

    Add root view
    Add a root view with a greeting and a link to the Swagger documentation.

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 
 app = FastAPI(
     title="Movie Catalog",
 )
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "World",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": f"Hello {name}!",
+        "docs": str(docs_url),
+    }
