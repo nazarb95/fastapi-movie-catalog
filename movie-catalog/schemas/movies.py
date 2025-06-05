171df7fb39241b4ae9385ba7d83b2fc69bca7e57
Nazar <naz95naz@outlook.com>
2025-06-06 02:38:23 +0300

    Create MovieCreate Schema

--- movie-catalog/schemas/movies.py
+++ movie-catalog/schemas/movies.py
@@ -1,17 +1,32 @@
+from typing import Annotated
+
+from annotated_types import Len, Ge, Le
 from pydantic import BaseModel
 
 
 class MovieBase(BaseModel):
-    id: int
     title: str
     description: str
     year: int
     genre: str
 
 
+class MovieCreate(MovieBase):
+    """
+    Model for movie creation
+    """
+
+    title: Annotated[str, Len(min_length=1, max_length=70)]
+    description: Annotated[str, Len(min_length=10, max_length=250)]
+    year: Annotated[int, Ge(1900), Le(2026)]
+    genre: Annotated[str, Len(min_length=1, max_length=25)]
+
+
 class Movie(MovieBase):
     """
     Movie Model
     """
+
+    id: int
 
     pass
