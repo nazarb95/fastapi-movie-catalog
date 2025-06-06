dfcc22345a9f05d1f525f2c8860dc8a53d3a4a3e
Nazar <naz95naz@outlook.com>
2025-06-06 13:32:06 +0300

    Added more nested routers
    Added more nesting for views: list and details

---
+++ movie-catalog/api/api_v1/movies/views/list_views.py
@@ -0,0 +1,39 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+
+from api.api_v1.movies.dependencies import (
+    exists_slug_movie,
+)
+from api.api_v1.movies.crud import storage
+from schemas.movies import (
+    Movie,
+    MovieCreate,
+)
+
+router = APIRouter(
+    prefix="/movies",
+    tags=["Movies"],
+)
+
+
+@router.get(
+    "/",
+    response_model=list[Movie],
+)
+def read_movies_list() -> list[Movie]:
+    return storage.get()
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    movie: Annotated[
+        MovieCreate,
+        Depends(exists_slug_movie),
+    ],
+) -> Movie:
+    return storage.create(movie_in=movie)
