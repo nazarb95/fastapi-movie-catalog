264c0d8e6f213de7c3cd3a9339df468035b83c3c
Nazar <naz95naz@outlook.com>
2025-06-06 17:07:20 +0300

    Create MoviePartialUpdate

--- movie-catalog/schemas/movies.py
+++ movie-catalog/schemas/movies.py
@@ -4,15 +4,35 @@ from annotated_types import Len, Ge, Le
 from pydantic import BaseModel
 
 
+TitleString = Annotated[
+    str,
+    Len(min_length=1, max_length=70),
+]
+SlugString = Annotated[
+    str,
+    Len(min_length=1, max_length=70),
+]
+DescriptionString = Annotated[
+    str,
+    Len(min_length=10, max_length=250),
+]
+YearInt = Annotated[
+    int,
+    Ge(1900),
+    Le(2026),
+]
+GenreString = Annotated[
+    str,
+    Len(min_length=1, max_length=25),
+]
+
+
 class MovieBase(BaseModel):
 
     title: str
-    description: Annotated[
-        str,
-        Len(min_length=10, max_length=250),
-    ] = ""
-    year: int
-    genre: str
+    description: DescriptionString = ""
+    year: YearInt
+    genre: GenreString
 
 
 class MovieCreate(MovieBase):
@@ -20,19 +40,10 @@ class MovieCreate(MovieBase):
     Model for movie creation
     """
 
-    slug: Annotated[
-        str,
-        Len(min_length=1, max_length=70),
-    ]
-    title: Annotated[
-        str,
-        Len(min_length=1, max_length=70),
-    ]
-    year: Annotated[int, Ge(1900), Le(2026)]
-    genre: Annotated[
-        str,
-        Len(min_length=1, max_length=25),
-    ]
+    slug: SlugString
+    title: TitleString
+    year: YearInt
+    genre: GenreString
 
 
 class MovieUpdate(MovieBase):
@@ -40,10 +51,18 @@ class MovieUpdate(MovieBase):
     Model for updating movie information
     """
 
-    description: Annotated[
-        str,
-        Len(min_length=10, max_length=250),
-    ]
+    description: DescriptionString
+
+
+class MoviePartialUpdate(MovieBase):
+    """
+    Model for partial updating movie information
+    """
+
+    title: TitleString | None = None
+    description: DescriptionString | None = None
+    year: YearInt | None = None
+    genre: GenreString | None = None
 
 
 class Movie(MovieBase):
