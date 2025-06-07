7da500cf03c684ea9b3de7a9453f78a3059b351f
Nazar <naz95naz@outlook.com>
2025-06-08 01:48:44 +0300

    Create abstract users helper

---
+++ movie-catalog/api/api_v1/auth/services/users_helper.py
@@ -0,0 +1,54 @@
+import secrets
+from abc import ABC, abstractmethod
+
+
+class AbstractUsersHelper(ABC):
+    @abstractmethod
+    def get_user_password(
+        self,
+        username: str,
+    ) -> str | None:
+        """
+        Finds the password from the passed username.
+
+        Returns the password if there is one.
+
+        :param username: username
+        :return: password by user, if found
+        """
+        pass
+
+    @classmethod
+    def check_passwords_match(
+        cls,
+        password1: str,
+        password2: str,
+    ) -> bool:
+        """
+        Checking passwords for a match.
+
+        :param password1: first password
+        :param password2: second password
+        :return:Boolean value of the comparison result
+        """
+        return password1 == password2
+
+    def validate_user_password(
+        self,
+        username: str,
+        password: str,
+    ):
+        """
+        Check if the password is valid.
+
+        :param username: whose password to check
+        :param password: passed password. Check against the one in the database
+        :return: True if they match, otherwise False
+        """
+        db_password = self.get_user_password(username)
+        if db_password is None:
+            return False
+        return self.check_passwords_match(
+            password1=password,
+            password2=db_password,
+        )
