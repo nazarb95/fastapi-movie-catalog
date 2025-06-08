812d9f84df2c9b006464fc3c4f3c0991a6381895
Nazar <naz95naz@outlook.com>
2025-06-08 18:43:44 +0300

    Create cli helper manage.py

---
+++ movie-catalog/commands/hello.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+
+import typer
+from rich import print
+
+app = typer.Typer(
+    no_args_is_help=True,
+    rich_markup_mode="rich",
+)
+
+
+@app.command(help="Greet user by [bold]name[/bold].")
+def hello(
+    name: Annotated[
+        str,
+        typer.Argument(help="Name to greet"),
+    ],
+):
+    print(f"[bold]Hello, [green]{name}[/green]![/bold]")
