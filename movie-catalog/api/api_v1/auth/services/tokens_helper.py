72c99706d6e510494079babef099501be2ca4380
Nazar <naz95naz@outlook.com>
2025-06-08 23:35:58 +0300

    Create "delete token" method

--- movie-catalog/api/api_v1/auth/services/tokens_helper.py
+++ movie-catalog/api/api_v1/auth/services/tokens_helper.py
@@ -30,6 +30,13 @@ class AbstractTokensHelper(ABC):
         Get all tokens.
         """
 
+    def delete_token(self, token: str) -> None:
+        """
+        Delete a token.
+        :param token:
+        :return:
+        """
+
     @classmethod
     def generate_token(cls) -> str:
         return secrets.token_urlsafe(16)
