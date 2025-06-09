ab7871d1fec17f746e494732e76a62127b0d5592
Nazar <naz95naz@outlook.com>
2025-06-09 19:52:22 +0300

    Tests for CRUD updates + setUp and tearDown

--- movie-catalog/testing/test_api/test_api_v1/test_movies/test_crud.py
+++ movie-catalog/testing/test_api/test_api_v1/test_movies/test_crud.py
@@ -1,4 +1,10 @@
+import random
+import string
 from os import getenv
+from unittest import TestCase
+
+from api.api_v1.movies.crud import storage
+from schemas.movies import Movie, MovieCreate, MoviePartialUpdate, MovieUpdate
 
 if getenv("TESTING") != "1":
     raise OSError(  # noqa: TRY003
@@ -6,5 +12,62 @@ if getenv("TESTING") != "1":
     )
 
 
-def total(a: int, b: int) -> int:
-    return a + b
+class MovieStorageUpdateTestCase(TestCase):
+    def setUp(self) -> None:
+        self.movie = self.create_movie()
+
+    def tearDown(self) -> None:
+        storage.delete(self.movie)
+
+    def create_movie(self) -> Movie:
+        movie_in = MovieCreate(
+            slug="".join(
+                random.choices(
+                    string.ascii_letters,
+                    k=8,
+                ),
+            ),
+            title="Test Movie",
+            description="Test Movie Description",
+            year=2020,
+            genre="Action",
+        )
+        return storage.create(movie_in=movie_in)
+
+    def test_update(self) -> None:
+        movie_update = MovieUpdate(
+            **self.movie.model_dump(),
+        )
+        source_description = self.movie.description
+        movie_update.description *= 2
+        updated_movie = storage.update(
+            movie=self.movie,
+            movie_in=movie_update,
+        )
+        self.assertNotEqual(
+            source_description,
+            updated_movie.description,
+        )
+        self.assertEqual(
+            movie_update,
+            MovieUpdate(**updated_movie.model_dump()),
+        )
+
+    def test_update_partial(self) -> None:
+        movie_partial_update = MoviePartialUpdate(
+            description=self.movie.description * 2,
+        )
+        source_description = self.movie.description
+        updated_movie = storage.update_partial(
+            movie=self.movie,
+            movie_in=movie_partial_update,
+        )
+
+        self.assertNotEqual(
+            source_description,
+            updated_movie.description,
+        )
+        self.assertEqual(
+            movie_partial_update.description,
+            updated_movie.description,
+        )
