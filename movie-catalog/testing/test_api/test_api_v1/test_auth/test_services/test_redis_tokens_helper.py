30a23d4e1f98b7e044ebfa64b2e85ce96b1b9ea7
Nazar <naz95naz@outlook.com>
2025-06-09 19:14:37 +0300

    Test redis tokens helper with database

---
+++ movie-catalog/testing/test_api/test_api_v1/test_auth/test_services/test_redis_tokens_helper.py
@@ -0,0 +1,16 @@
+from os import getenv
+from unittest import TestCase
+
+from api.api_v1.auth.services import redis_tokens
+
+if getenv("TESTING") != "1":
+    raise OSError(  # noqa: TRY003
+        "Environment is not ready for testing",  # noqa: EM101
+    )
+
+
+class RedisTokensHelperTestCase(TestCase):
+    def test_generate_and_save_token(self) -> None:
+        new_token = redis_tokens.generate_and_save_token()
+        expected_exists = True
+        self.assertEqual(expected_exists, redis_tokens.token_exists(new_token))
