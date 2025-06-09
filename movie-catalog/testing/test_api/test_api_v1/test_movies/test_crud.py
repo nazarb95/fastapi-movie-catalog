a3fe33f591f2423b93cd9cec1db783a2c562ea5a
Nazar <naz95naz@outlook.com>
2025-06-09 19:04:13 +0300

    Testing environment

--- movie-catalog/testing/test_api/test_api_v1/test_movies/test_crud.py
+++ movie-catalog/testing/test_api/test_api_v1/test_movies/test_crud.py
@@ -1,15 +1,10 @@
-import random
-from unittest import TestCase
+from os import getenv
+
+if getenv("TESTING") != "1":
+    raise OSError(  # noqa: TRY003
+        "Environment is not ready for testing",  # noqa: EM101
+    )
 
 
 def total(a: int, b: int) -> int:
     return a + b
-
-
-class TotalTestCase(TestCase):
-    def test_total(self) -> None:
-        num_a = random.randint(1, 100)
-        num_b = random.randint(1, 100)
-        result = total(num_a, num_b)
-        expect_result = num_a + num_b
-        self.assertEqual(expect_result, result)
